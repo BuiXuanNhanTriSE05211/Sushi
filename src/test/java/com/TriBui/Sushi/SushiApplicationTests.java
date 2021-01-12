f45d6e7a33fb0cefcde935392af05f3d779cffff
TRICOMPUTER\This MC <nhantri702@gmail.com>
2021-01-12 21:51:21 +0700

    firstcommit

---
+++ src/test/java/com/TriBui/Sushi/SushiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TriBui.Sushi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SushiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
